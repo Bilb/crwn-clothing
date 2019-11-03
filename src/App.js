d8132be7dcbd7e62617cf3df5f612071e275ca06
Audric Ackermann <audric.ackermann@smile.fr>
2019-11-03 19:06:13 +1100

    Adding header component and links to home, shop apge and  contact

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.components";
+import Header from "./components/header/header.components";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
