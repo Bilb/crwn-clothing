f27103c43dccfa7d460ad074bd27cfe896413f5c
Audric Ackermann <audric.ackermann@smile.fr>
2019-11-09 14:40:22 +1100

    added checkout page and checkout component. updated cart to use selector

--- src/redux/cart/cart.selector.js
+++ src/redux/cart/cart.selector.js
@@ -8,11 +8,25 @@ export const selectCartItems = createSelector(
   cart => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+);
+
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   cartItems =>
     cartItems.reduce(
       (accumulated, cartItem) => accumulated + cartItem.quantity,
+      0
+    )
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumulated, item) => accumulated + item.price * item.quantity,
       0
     )
 );
